81e52beed46eabf0665a08f98de5312efee86d4f
HyunJoonKang <steady.kang27@gmail.com>
2023-03-16 02:28:56 +0900

    feat: dynamic-user-input

--- src/component/CodeInput.js
+++ src/component/CodeInput.js
@@ -1,14 +1,15 @@
 import CodeMirror from "@uiw/react-codemirror";
 import { javascript } from "@codemirror/lang-javascript";
 
-export default function CodeInput() {
+export default function CodeInput({ value, onChange }) {
   return (
     <div>
       <div>코드 입력</div>
       <CodeMirror
-        value="CodeInput"
+        value={value}
         height="200px"
         extensions={[javascript({ jsx: false })]}
+        onChange={onChange}
       />
     </div>
   );

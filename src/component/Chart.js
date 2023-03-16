72bf8339a179f93ff431c7b24da733ca0bdf7538
isinthesky <hijde@naver.com>
2023-03-16 22:32:29 +0900

    faet: chart playback control(play, pause, stop)

--- src/component/Chart.js
+++ src/component/Chart.js
@@ -1,12 +1,53 @@
 import React, { useEffect, useState } from "react";
 import LineChart from "../utils/msChart";
+import styled from "styled-components";
+
+const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  color: white;
+  font-size: 18px;
+  height: 30px;
+  width: 100px;
+  background-color: #1e90ff;
+  margin: 10px;
+  border: none;
+  border-radius: 20px;
+`;
+
+const Controller = styled.div`
+  display: flex;
+`;
 
 export default function Chart() {
   const [chart, setChart] = useState();
 
   useEffect(() => {
-    setChart(new LineChart("lineChart", 5));
+    setChart(new LineChart("lineChart", 5000));
   }, []);
 
-  return <canvas id="lineChart" width="400px" height="300px"></canvas>;
+  function handleChartPlay() {
+    chart.playback();
+  }
+
+  function handleChartPause() {
+    chart.pause();
+  }
+
+  function handleChartStop() {
+    chart.stop();
+  }
+
+  return (
+    <>
+      <canvas id="lineChart" width="400px" height="300px"></canvas>
+      <Controller>
+        <Button onClick={handleChartPlay}>시작</Button>
+        <Button onClick={handleChartPause}>일시정지</Button>
+        <Button onClick={handleChartStop}>종료</Button>
+      </Controller>
+    </>
+  );
 }

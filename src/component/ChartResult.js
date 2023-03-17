4e53338160213a534825bf906bc8314d7cc43e00
lin01-cloud <lin01.dev@gmail.com>
2023-03-18 01:36:00 +0900

    style: chartResult 컴포넌트 스타일 작업 완료

--- src/component/ChartResult.js
+++ src/component/ChartResult.js
@@ -1,4 +1,40 @@
-import React, { useState } from "react";
+import React from "react";
+import styled from "styled-components";
+
+const StyledWrapper = styled.div`
+  ul {
+    padding: 20px;
+    height: 40vh;
+
+    background-color: white;
+  }
+
+  .title {
+    text-align: center;
+    line-height: 10vh;
+    height: 10vh;
+
+    font-size: 1.6rem;
+    font-weight: 900;
+  }
+
+  .info {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    font-size: 1rem;
+    height: 8vh;
+    margin-top: 1vh;
+
+    border-bottom: 0.2px solid gray;
+  }
+
+  .name::before {
+    content: "🔷";
+    margin-right: 10px;
+  }
+`;
 
 export default function ChartResult({ data }) {
   const totalRunTime = data ? data[data.length - 1].timeStamp : "";
@@ -14,20 +50,22 @@ export default function ChartResult({ data }) {
     : "";
 
   return (
-    <ul>
-      <li className="title">React</li>
-      <li>
-        <div className="name">TOTAL RUN TIME</div>
-        <div className="data">{totalRunTime}</div>
-      </li>
-      <li>
-        <div className="name">MAX MEMORY</div>
-        <div className="data">{max_Memory}</div>
-      </li>
-      <li>
-        <div className="name">MIN MEMORY</div>
-        <div className="data">{min_Memory}</div>
-      </li>
-    </ul>
+    <StyledWrapper>
+      <ul>
+        <li className="title">RESULT</li>
+        <li className="info">
+          <div className="name">TOTAL RUN TIME</div>
+          <div className="data">{totalRunTime}</div>
+        </li>
+        <li className="info">
+          <div className="name">MAX MEMORY</div>
+          <div className="data">{max_Memory}</div>
+        </li>
+        <li className="info">
+          <div className="name">MIN MEMORY</div>
+          <div className="data">{min_Memory}</div>
+        </li>
+      </ul>
+    </StyledWrapper>
   );
 }

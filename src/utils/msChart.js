b740fbd039dad9a4c750236b8d29229c5f75dabb
lin01-cloud <lin01.dev@gmail.com>
2023-03-21 17:31:16 +0900

    fix: Y축 바깥에서node에 모달 생성 수정 완료

--- src/utils/msChart.js
+++ src/utils/msChart.js
@@ -6,7 +6,7 @@ const Y_PADDING = 50;
 const TOP_PADDING = 15;
 const VIEW_NODE_COUNT = 13;
 const Y_TICK_COUNT = 7;
-const NODE_RADIUS = 5;
+const NODE_RADIUS = 2;
 
 export default class LineChart {
   constructor(id, data, durationTime) {
@@ -36,12 +36,15 @@ export default class LineChart {
     this.circle = [];
     this.snapshotCircle = [];
 
-    // node 모달 창 생성
     this.canvas.addEventListener("mousemove", (e) => {
       const cursorPositionX = e.clientX - this.ctx.canvas.offsetLeft;
       const cursorPositionY = e.clientY - this.ctx.canvas.offsetTop;
 
-      if (this.snapshotCircle.length > 0) {
+      if (
+        this.snapshotCircle.length > 0 &&
+        cursorPositionX > Y_PADDING &&
+        cursorPositionY < this.chartHeight + TOP_PADDING
+      ) {
         this.snapshotCircle.forEach((item) => {
           if (item.isMouseOver(cursorPositionX, cursorPositionY)) {
             const modal = document.getElementById(`${item.modal.id}`);
@@ -62,7 +65,6 @@ export default class LineChart {
         });
       }
     });
-    // heightPixelWeights 얻기
     this.parseMemoryArray();
 
     return this;
@@ -152,39 +154,44 @@ export default class LineChart {
     ctx.stroke();
 
     /* Y축 좌측으로 노드가
-    그려지지 않게 해당 부분을 가림 */
+    그려지지 않게 함 */
     ctx.save();
     ctx.beginPath();
     ctx.rect(Y_PADDING, 0, chartWidth, canvasHeight);
     ctx.clip();
 
-    /* 동적으로 움직이는 차트 내
-    한 장면의 노드 개수만큼 Circle 객체 생성 */
-    const offset = 25;
-    this.snapshotCircle = this.data
-      .filter(
-        (item) =>
-          item.timeStamp > xDistance * BigInt(this.currentPosition - offset) &&
-          item.timeStamp <
-            xDistance * BigInt(this.currentPosition + VIEW_NODE_COUNT + offset),
-      )
-      .map((item) => {
-        const xPosition =
-          (this.chartWidth / (Number(xDistance) * VIEW_NODE_COUNT)) *
-            Number(item.timeStamp) -
-          (this.chartWidth / VIEW_NODE_COUNT) * this.currentPosition +
-          offset;
-        const yPosition =
-          TOP_PADDING +
-          this.chartHeight -
-          this.heightPixelWeights * item.usedMemory;
+    const createCircleInChart = () => {
+      const offset = 25;
+      const merginNode = 5;
+      this.snapshotCircle = this.data
+        .filter(
+          (item) =>
+            item.timeStamp >
+              xDistance * BigInt(this.currentPosition - merginNode) &&
+            item.timeStamp <
+              xDistance *
+                BigInt(this.currentPosition + VIEW_NODE_COUNT + merginNode),
+        )
+        .map((item) => {
+          const xPosition =
+            (this.chartWidth / (Number(xDistance) * VIEW_NODE_COUNT)) *
+              Number(item.timeStamp) -
+            (this.chartWidth / VIEW_NODE_COUNT) * this.currentPosition +
+            offset;
+          const yPosition =
+            TOP_PADDING +
+            this.chartHeight -
+            this.heightPixelWeights * item.usedMemory;
 
-        return new Circle(xPosition, yPosition, NODE_RADIUS, ctx, item);
-      });
+          return new Circle(xPosition, yPosition, NODE_RADIUS, ctx, item);
+        });
+    };
+    createCircleInChart();
 
-    // 동적으로 움직이는 한 장면에 노드를 표현하였습니다.
-    // FIXME: 노드가 x축과 겹치는 현상 해결 필요.
     ctx.beginPath();
+
+    /* 노드들을 연결하는 선 */
+    // FIXME: 노드가 x축과 겹치는 현상 해결 필요
     this.snapshotCircle.forEach((item, index) => {
       const xPosition = item.x;
       const yPosition = item.y;
@@ -197,7 +204,7 @@ export default class LineChart {
       item.draw(color.chartDot);
     });
 
-    // x축 좌표 ns 표현
+    /* x축 좌표 ns 표현 */
     for (let index = 0; index < VIEW_NODE_COUNT; index += 1) {
       const xPosition = (this.chartWidth / VIEW_NODE_COUNT) * (index + 1);
 
@@ -210,7 +217,6 @@ export default class LineChart {
       );
     }
 
-    // FIXME: Circle method인 reDraw()로 인한 색변경 오류 발견. 해결 필요
     ctx.stroke();
     ctx.restore();
   };

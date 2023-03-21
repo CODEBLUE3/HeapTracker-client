0b611ae70c681e22511c9aa787082b5789d66450
isinthesky <hijde@naver.com>
2023-03-21 19:33:10 +0900

    fix: ChartResult value 표현 개선

--- src/component/ChartResult.js
+++ src/component/ChartResult.js
@@ -3,9 +3,11 @@ import styled from "styled-components";
 import { color, style } from "../styles/styleCode";
 
 const StyledWrapper = styled.div`
+  height: 30%;
+
   ul {
     padding: 20px;
-    height: 40vh;
+    height: 30vh;
 
     background-color: ${color.defaultBoxBackground};
     border-radius: ${style.borderRadius};
@@ -13,8 +15,8 @@ const StyledWrapper = styled.div`
 
   .title {
     text-align: center;
-    line-height: 10vh;
-    height: 10vh;
+    line-height: 4vh;
+    height: 4vh;
 
     font-size: 1.6rem;
     font-weight: 900;
@@ -26,15 +28,25 @@ const StyledWrapper = styled.div`
     align-items: center;
 
     font-size: 1rem;
-    height: 8vh;
+    height: 4vh;
     margin-top: 1vh;
 
     border-bottom: 0.2px solid gray;
   }
 
+  .name {
+    margin-left: 4vh;
+    width: 180px;
+  }
+
   .name::before {
     content: "🔷";
     margin-right: 10px;
+  }
+
+  .data {
+    width: 180px;
+    text-align: center;
   }
 `;
 
@@ -44,24 +56,59 @@ export default function ChartResult({ resultData }) {
       <ul>
         <li className="title">RESULT</li>
         <li className="info">
-          <div className="name">TOTAL RUN TIME</div>
+          <div className="name">RUN TIME</div>
           <div className="data">
-            {Number(resultData.duration)}ns,{" "}
-            {Math.floor(Number(resultData.duration) / 1000000)}
+            {Number(resultData.duration ? resultData.duration : 0)
+              .toString()
+              .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}
+            us,{" "}
+            {Math.floor(
+              (Number(resultData.duration ? resultData.duration : 0) / 1000000)
+                .toString()
+                .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ","),
+            )}
             ms
           </div>
         </li>
         <li className="info">
           <div className="name">MAX MEMORY</div>
-          <div className="data">{resultData.maxMemory}bytes</div>
+          <div className="data">
+            {(resultData.maxMemory ? resultData.maxMemory : 0)
+              .toString()
+              .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}
+            bytes
+          </div>
         </li>
         <li className="info">
           <div className="name">MIN MEMORY</div>
-          <div className="data">{resultData.minMemory}bytes</div>
+          <div className="data">
+            {(resultData.minMemory ? resultData.minMemory : 0)
+              .toString()
+              .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}
+            bytes
+          </div>
         </li>
         <li className="info">
-          <div className="name">TOTAL NODE COUNT</div>
-          <div className="data">{resultData.count}</div>
+          <div className="name">USED MEMORY</div>
+          <div className="data">
+            {(resultData.maxMemory
+              ? resultData.maxMemory
+              : 0 - resultData.minMemory
+              ? resultData.minMemory
+              : 0
+            )
+              .toString()
+              .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}
+            bytes
+          </div>
+        </li>
+        <li className="info">
+          <div className="name">NODE COUNT</div>
+          <div className="data">
+            {(resultData.count ? resultData.count : 0)
+              .toString()
+              .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}
+          </div>
         </li>
       </ul>
     </StyledWrapper>

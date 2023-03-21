490e1ab60cf1fc80bc9136fbbc2d405f6a5d8ff4
isinthesky <hijde@naver.com>
2023-03-21 21:55:34 +0900

    design: naming,  css수정

--- src/component/CodeExecution.js
+++ src/component/CodeExecution.js
@@ -1,10 +1,11 @@
 import styled from "styled-components";
 import CodeMirror from "@uiw/react-codemirror";
+import { style } from "../styles/styleCode";
 import { javascript } from "@codemirror/lang-javascript";
 
 const CodeContainer = styled.div`
   flex-direction: column;
-  margin: 20px 5px;
+  margin: 20px ${style.defaultComponentMargin};
   width: 85%;
   height: 70px;
 `;
--- src/styles/styleCode.js
+++ src/styles/styleCode.js
@@ -1,5 +1,5 @@
 const color = {
-  defaultBackground: "#fAfAfA",
+  defaultBackground: "#F0F0F0",
   defaultBoxBackground: "#FFFFFF",
   defaultButton: "#1E90FF",
   chartModal: "#DFDFE6",
@@ -9,6 +9,10 @@ const color = {
 
 const style = {
   borderRadius: "10px",
+  defaultComponentMargin: "5px",
+  controlButtonMargin: "10px",
+  controlButtonSize: "1rem",
+  buttonFontSize: "1.3rem",
 };
 
 export { color, style };

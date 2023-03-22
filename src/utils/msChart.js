c2fc0be743cd90b2c38b84a6e1ed03d94d295e12
isinthesky <hijde@naver.com>
2023-03-22 21:10:52 +0900

    style: msChart-lineChart Class 코드정리, Circle -> ChartNode 파일명 변경

--- src/utils/msChart.js
+++ src/utils/msChart.js
@@ -1,4 +1,4 @@
-import Circle from "./circle";
+import ChartNode from "./ChartNode";
 import { color } from "../styles/styleCode";
 
 const X_PADDING = 25;
@@ -10,21 +10,15 @@ const NODE_RADIUS = 4;
 const CHART_MARGIN_NODE = 5;
 
 export default class LineChart {
-  constructor(id, data, durationTime) {
-    this.data = [...data];
+  constructor(id) {
     this.canvas = document.getElementById(id);
     this.ctx = this.canvas.getContext("2d");
-
-    this.xDistance =
-      this.data[this.data.length - 1].timeStamp / this.data.length;
-    this.baseMemory = this.data[0].usedMemory;
 
     this.canvasWidth = this.canvas.clientWidth;
     this.canvasHeight = this.canvas.clientHeight;
     this.chartWidth = this.canvasWidth - Y_PADDING;
     this.chartHeight = this.canvasHeight - X_PADDING - TOP_PADDING;
 
-    this.chartDurationTime = durationTime;
     this.excuteDurationTime = 0;
     this.intervalID = 0;
     this.currentPosition = 0;
@@ -33,13 +27,9 @@ export default class LineChart {
     this.minMemoryText = Infinity;
     this.maxMemoryValue = 0;
     this.minMemoryValue = Infinity;
-    this.maxTimeValue = 0;
-    this.minTimeValue = Infinity;
     this.heightPixelWeights = 0;
-    this.widthPixelWeights = 0;
 
-    this.circle = [];
-    this.snapshotCircle = [];
+    this.snapshotNodes = [];
     this.removeCodeTypeArray = ["Statement", "Declaration", "Expression"];
 
     this.canvas.addEventListener("mousemove", (e) => {
@@ -49,13 +39,13 @@ export default class LineChart {
       const cursorPositionY = e.clientY - this.ctx.canvas.offsetTop;
 
       if (
-        this.snapshotCircle.length > 0 &&
+        this.snapshotNodes.length > 0 &&
         cursorPositionX > Y_PADDING &&
         cursorPositionY < this.chartHeight + TOP_PADDING
       ) {
-        this.snapshotCircle.forEach((item) => {
-          if (item.isMouseOver(cursorPositionX, cursorPositionY)) {
-            const modal = document.getElementById(`${item.modal.id}`);
+        this.snapshotNodes.forEach((node) => {
+          if (node.isMouseOver(cursorPositionX, cursorPositionY)) {
+            const modal = document.getElementById(`${node.modal.id}`);
 
             modal.style.visibility = "visible";
             modal.style.position = "absolute";
@@ -65,40 +55,66 @@ export default class LineChart {
             modal.style.borderRadius = "10px";
             modal.style.padding = "10px 20px";
             modal.style.fontSize = "1rem";
-            modal.innerText = `${item.data.count}th node
+            modal.innerText = `${node.data.count}th node
             ${
-              item.data.codeType
+              node.data.codeType
                 ? `Type : ` +
                   this.removeCodeTypeArray
                     .map((type) => {
-                      if (item.data.codeType.includes(type)) {
-                        return item.data.codeType.replace(type, "");
+                      if (node.data.codeType.includes(type)) {
+                        return node.data.codeType.replace(type, "");
                       }
                     })
-                    .filter((item) => item)
-                : `Code Position : ` + item.data.codePosition
+                    .filter((node) => node)
+                : `Code Position : ` + node.data.codePosition
             }
-            Used Memory : ${item.data.usedMemory}
-            Time : ${Math.floor(Number(item.data.timeStamp)) / 1000} us`;
+            Used Memory : ${node.data.usedMemory}
+            Time : ${Math.floor(Number(node.data.timeStamp)) / 1000} us`;
 
-            item.draw(color.chartDotHover);
+            node.draw(color.chartDotHover);
           } else {
-            item.draw(color.chartDot);
+            node.draw(color.chartDot);
           }
         });
       }
     });
-    this.parseMemoryArray();
 
     return this;
   }
 
+  setData = (data, durationTime) => {
+    this.data = [...data];
+
+    if (!this.data.length) return;
+
+    this.baseMemory = this.data[0].usedMemory;
+    this.chartDurationTime = durationTime;
+    this.excuteDurationTime = this.data.length;
+
+    this.xDistance =
+      this.data[this.data.length - 1].timeStamp / this.data.length;
+
+    this.data.forEach((item) => {
+      this.minMemoryText = Math.min(this.minMemoryText, item.usedMemory);
+      this.maxMemoryText = Math.max(this.maxMemoryText, item.usedMemory);
+    });
+
+    this.data.forEach((item) => {
+      this.minMemoryValue = Math.min(this.minMemoryValue, item.usedMemory);
+      this.maxMemoryValue = Math.max(this.maxMemoryValue, item.usedMemory);
+    });
+
+    this.heightPixelWeights =
+      (this.chartHeight - Y_PADDING) /
+      (this.maxMemoryValue - this.minMemoryValue);
+  };
+
   playback = () => {
     if (this.intervalID < 1) {
-      this.snapshotCircle.forEach((item) => {
-        item.clearModal();
+      this.snapshotNodes.forEach((node) => {
+        node.clearModal();
       });
-      this.snapshotCircle = [];
+      this.snapshotNodes = [];
 
       this.intervalID = setInterval(() => {
         this.updateData();
@@ -107,7 +123,6 @@ export default class LineChart {
   };
 
   pause = () => {
-    this.circle.every((item) => item.draw());
     clearInterval(this.intervalID);
     this.intervalID = 0;
   };
@@ -116,26 +131,8 @@ export default class LineChart {
     clearInterval(this.intervalID);
     this.intervalID = 0;
     this.currentPosition = 0;
-  };
-
-  parseMemoryArray = () => {
-    if (this.data.length) {
-      this.data.forEach((item) => {
-        this.minMemoryText = Math.min(this.minMemoryText, item.usedMemory);
-        this.maxMemoryText = Math.max(this.maxMemoryText, item.usedMemory);
-      });
-
-      this.excuteDurationTime = this.data.length;
-
-      this.data.forEach((item) => {
-        this.minMemoryValue = Math.min(this.minMemoryValue, item.usedMemory);
-        this.maxMemoryValue = Math.max(this.maxMemoryValue, item.usedMemory);
-      });
-
-      this.heightPixelWeights =
-        (this.chartHeight - Y_PADDING) /
-        (this.maxMemoryValue - this.minMemoryValue);
-    }
+    this.snapshotNodes.length = 0;
+    this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight - X_PADDING);
   };
 
   drawChart = () => {
@@ -182,9 +179,9 @@ export default class LineChart {
 
     ctx.beginPath();
 
-    const createCircleInChart = () => {
+    const createNodeInChart = () => {
       const offset = 25;
-      this.snapshotCircle = this.data
+      this.snapshotNodes = this.data
         .filter(
           (item) =>
             item.timeStamp >
@@ -203,24 +200,24 @@ export default class LineChart {
             this.chartHeight -
             this.heightPixelWeights * (item.usedMemory - this.baseMemory);
 
-          return new Circle(xPosition, yPosition, NODE_RADIUS, ctx, item);
+          return new ChartNode(xPosition, yPosition, NODE_RADIUS, ctx, item);
         });
     };
 
-    createCircleInChart();
+    createNodeInChart();
 
     /* 노드들을 연결하는 선 */
     // FIXME: 노드가 x축과 겹치는 현상 해결 필요
-    this.snapshotCircle.forEach((item, index) => {
-      const xPosition = item.x;
-      const yPosition = item.y;
+    this.snapshotNodes.forEach((node, index) => {
+      const xPosition = node.x;
+      const yPosition = node.y;
 
       ctx.lineTo(xPosition, yPosition);
       ctx.stroke();
       ctx.save();
 
       ctx.moveTo(xPosition, yPosition);
-      item.draw(color.chartDot);
+      node.draw(color.chartDot);
     });
 
     /* x축 좌표 ns 표현 */
@@ -243,10 +240,8 @@ export default class LineChart {
   };
 
   updateData = () => {
-    if (!this.data.length) {
-      return;
-    }
-    this.circle.length = 0;
+    if (!this.data.length) return;
+
     this.drawChart();
     this.currentPosition += 1;
 

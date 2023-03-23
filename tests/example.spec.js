8c856ef24e793cfb4489e871ac5ed6534b10babf
HyunJoonKang <steady.kang27@gmail.com>
2023-03-23 20:24:32 +0900

    set: playright init

---
+++ tests/example.spec.js
@@ -0,0 +1,18 @@
+// @ts-check
+const { _electron: electron } = require("playwright");
+const { test, expect } = require("@playwright/test");
+
+let page;
+
+test("launch app", async () => {
+  const electronApp = await electron.launch({
+    args: [__dirname + "/../src/electron/main.js"],
+  });
+
+  page = await electronApp.firstWindow();
+  const title = await page.title();
+  expect(title).toBe("Heap Tracker");
+
+  // close app
+  await electronApp.close();
+});
